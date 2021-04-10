d2d40ea25e5cc485ab2f2e84c1bdff783f80b9f7
user.name"bria222 <user.email"briantechologies.co.ke>
2021-04-11 01:01:10 +0300

    first commit

--- application_app/views.py
+++ application_app/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User,auth
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from .models import *
 def index(request):
     dataa= Details.objects.all()
     return render(request, 'application_app/index.html', {'dataa':dataa})
 
 def register(request):
-    if request.method == 'post':
-        first_name = request.post['first_name']
+    return render(request, 'application_app/register.html')
 
-
-    else:
-        return render(request, 'application_app/register.html')
+    
+       
